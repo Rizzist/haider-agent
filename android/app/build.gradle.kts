7cda00b2ceeb3e464aca053350e436778463356b
Rizzist <syedmraza99@gmail.com>
2026-08-25 23:51:11 +0330

    android: coroutines dep for transport/services layer

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
